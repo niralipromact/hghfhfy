72e24611ead600920fadb5e12e873e4eaf43d98a
niralipromact <nirali@promactinfo.com>
2014-12-31 11:54:42 +0530

    first

---
+++ DemoAngularApp/Scripts/MyNoteCtrl.js
@@ -0,0 +1,6 @@
+﻿app.controller("MyNoteCtrl", function ($scope) {
+    $scope.message = "Enter  Text here";
+    $scope.left = function () { return 160 - $scope.message.length; };
+    $scope.clear = function () { $scope.message = ""; };
+    $scope.save = function () { alert(" Just demo Application ...Note Saved"); };
+});
